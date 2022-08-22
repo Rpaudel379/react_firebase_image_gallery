670eb9931bfd8ffc3d0532a950ea91a317d5322e
Anish Paudel <Rpaudel379@gmail.com>
2022-08-22 20:15:48 +0530

    added environment variables for firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,7 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // ....
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
